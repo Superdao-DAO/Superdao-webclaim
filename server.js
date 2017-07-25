d900cd62e81c69bec76f8179e2b0ff7c8dce3642
panos <panos@superdao.io>
2017-07-25 11:27:47 +0300

    More refactor

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ http.createServer(function (req, res) {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
     path = url_parts.path;
-    path = path=="/"?'index.html':path.substring(1,path.length);
+    path = path == "/" ? 'index.html' : path.substring(1, path.length);
 
-    fs.readFile(path, "binary", function(err, file) {
-      if(err) {
-       res.writeHead(500, {"Content-Type": "text/plain"});
-       res.write(err + "\n");
-       res.end();
-       return;
-     }
+    fs.readFile(path, "binary", function (err, file) {
+        if (err) {
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            res.write(err + "\n");
+            res.end();
+            return;
+        }
 
-     res.writeHead(200,{"Content-Type": "text/html"});
-     res.write(file, "binary");
-     res.end();
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(file, "binary");
+        res.end();
     });
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');

db20c492f3772b8ca8d3234ae9fd0eea148dac95
precioustosin <precioustosin@hotmail.com>
2017-08-04 20:56:36 +0100

    inserted require statement for 'alertify'

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -7,6 +7,7 @@ import strings from './strings';
 
 const $ = require('jquery');
 const MobileDetect = require('mobile-detect');
+const alertify = require('alertify.js');
 
 let app; // eslint-disable-line no-unused-vars
 
